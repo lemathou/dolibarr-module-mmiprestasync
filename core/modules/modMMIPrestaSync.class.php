e3ea48dc9ac3272b814efa7df9ddd91290e37c01
Mathieu Moulin <mathieu@iprospective.fr>
2024-02-13 16:45:39 +0100

    Module depends update

--- core/modules/modMMIPrestaSync.class.php
+++ core/modules/modMMIPrestaSync.class.php
@@ -130,7 +130,7 @@ class modMMIPrestaSync extends DolibarrModules
 		// A condition to hide module
 		$this->hidden = false;
 		// List of module class names as string that must be enabled if this module is enabled. Example: array('always1'=>'modModuleToEnable1','always2'=>'modModuleToEnable2', 'FR1'=>'modModuleToEnableFR'...)
-		$this->depends = array('modMMICommon', 'modMMIProduct', 'modMMIFournisseurPrice', 'modMMIWorkflow');
+		$this->depends = array('modMMICommon', 'modMMIProduct', 'modMMIFournisseurPrice', 'modMMIPayments', 'modMMIWorkflow');
 		$this->requiredby = array(); // List of module class names as string to disable if this one is disabled. Example: array('modModuleToDisable1', ...)
 		$this->conflictwith = array(); // List of module class names as string this module is in conflict with. Example: array('modModuleToDisable1', ...)
 		$this->langfiles = array("mmiprestasync@mmiprestasync");
